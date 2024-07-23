e607fccab5ebbcecd270af284ca657e7f17fe003
selvimurugan2595 <selvimurugan2595@gmail.com>
2024-07-23 07:28:59 +0530

    project add

---
+++ src/main/java/NewClass.java
@@ -0,0 +1,12 @@
+import org.openqa.selenium.WebDriver;
+
+public class NewClass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		WebDriver driver;
+		
+	}
+
+}
---
+++ src/test/java/pack_sample/apack1.java
@@ -0,0 +1,10 @@
+package pack_sample;
+
+public class apack1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
